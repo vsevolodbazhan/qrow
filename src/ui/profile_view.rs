bce72141b005f45a42137bac8836f5da87b7f11f
Vsevolod Bazhan <work@bazhan.one>
2026-09-20 11:44:02 +0700

    feat(ui): rebuild connection settings form

--- src/ui/profile_view.rs
+++ src/ui/profile_view.rs
@@ -43,7 +43,7 @@ impl Qrow {
             let viewport = window.viewport_size();
             let height = (rem * 48.).min(viewport.height - rem * 4.);
             dialog
-                .title("Connection Settings")
+                .title("Connection settings")
                 .w(dialog_width(window))
                 .h(height)
                 .margin_top((viewport.height - height) / 2.)
